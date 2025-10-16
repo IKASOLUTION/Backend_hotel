fb429999c2f7553a84e283bddf089481fbb12eac
SOME Narcisse <some.a.narcisse@gmail.com>
2025-10-16 02:21:38 +0000

    Gestion du mapping de hotel

--- src/main/java/com/hotel/bf/dto/mapper/HotelMapper.java
+++ src/main/java/com/hotel/bf/dto/mapper/HotelMapper.java
@@ -1,12 +1,27 @@
 package com.hotel.bf.dto.mapper;
 
+import com.hotel.bf.domain.Commune;
 import com.hotel.bf.domain.Hotel;
+import com.hotel.bf.dto.CommuneDto;
 import com.hotel.bf.dto.HotelDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 /**
  * Mapper for the entity Site and its DTO SiteDTO.
  */
 
 @Mapper(componentModel = "spring")
-public interface HotelMapper extends EntityMapper<HotelDto, Hotel> {}
+public interface HotelMapper extends EntityMapper<HotelDto, Hotel> {
+    @Mapping(target = "commune", source = "commune", qualifiedByName = "communeId")
+    HotelDto toDto(Hotel s);
+
+    @Named("communeId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "libelle", source = "libelle")
+    @Mapping(target = "province", source = "province")
+    CommuneDto toDtoCommuneId(Commune commune);
+}
